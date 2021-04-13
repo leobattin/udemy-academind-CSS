03e0cbe34a2b469bc3926dfc2f1c1f34f72e4b12
Léo Battin <battinleo@gmail.com>
2021-04-13 21:50:26 +0200

    258: Transitions & 'display' - - Non-observable display workaround for transitions

--- shared.js
+++ shared.js
@@ -8,7 +8,10 @@ var modalCloseButton = document.querySelector('.modal__action--negative');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', () => {
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+        setTimeout(() => {
+            backdrop.classList.add('open');
+        }, 1);
         modal.classList.add('open');
     });
 }
@@ -16,17 +19,26 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
 if (modal) {
     modalCloseButton.addEventListener('click', () => {
         backdrop.classList.remove('open');
+        setTimeout(() => {
+            backdrop.style.display = 'none';
+        }, 200);
         modal.classList.remove('open');
     });
 }
 
 backdrop.addEventListener('click', () => {
     backdrop.classList.remove('open');
+    setTimeout(() => {
+        backdrop.style.display = 'none';
+    }, 200);
     if (modal) modal.classList.remove('open');
     mobileNav.classList.remove('open');
 });
 
 toggleButton.addEventListener('click', () => {
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(() => {
+        backdrop.classList.add('open');
+    }, 1);
     mobileNav.classList.add('open');
 });
